694806a69027c8f4237a7342c6b90ce3a7c89392
maxjacobzander <maxjacobzander@gmail.com>
2020-08-21 16:35:24 -0400

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,10 +19,3 @@ def long_planeteer_calls(array)
   end
   long_planeteer_calls(array)
 end
-
-# def find_the_cheese(array)
-#  cheese_types = ["cheddar", "gouda", "camembert"]
-#  array.find do |type|
-#   type.include?(cheese_types)
-#  end
-# end
