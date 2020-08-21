b0561030f3d92047ce9b7d063a078759dc20e9cf
maxjacobzander <maxjacobzander@gmail.com>
2020-08-21 16:51:38 -0400

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,6 +20,6 @@ end
 def find_the_cheese(array)
  cheese_types = ["cheddar", "gouda", "camembert"]
  array.find do |type|
-     type.include?(cheese_types)
+  cheese_types.include?(type)
   end
  end
