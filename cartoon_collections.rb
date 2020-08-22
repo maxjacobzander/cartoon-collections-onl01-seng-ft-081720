22d8cd9d5760efeccec0cccadb1256ff27fc094b
Max Zander <maxjacobzander@gmail.com>
2020-08-22 14:59:06 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,9 +17,19 @@ def long_planeteer_calls(array)
   array.any? {|word| word.length > 4 }
 end
 
+<<<<<<< HEAD
 def find_the_cheese(array)
  cheese_types = ["cheddar", "gouda", "camembert"]
  array.find do |type|
   cheese_types.include?(type)
   end
+<<<<<<< HEAD
  end
+=======
+=======
+#def find_the_cheese(array)
+  # the array below is here to help
+ # cheese_types = ["cheddar", "gouda", "camembert"]
+>>>>>>> 50c8fa65ca86df92b742b6fb68ee690812367ed9
+end
+>>>>>>> ba81787207bedc147c3e6f981d9021a30be94895
